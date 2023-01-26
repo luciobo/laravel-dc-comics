225b134e61768d5032972733b842e4988b10091f
luciobo <luciobo@icloud.com>
2023-01-27 00:11:32 +0100

    aggiunta tabella con ciclo foreach in comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,6 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="bg-green">
-    <h1>comics page</h1>
-@endsection
+    <section class="bg-green">
+        <h1 class="d-flex justify-content-between">
+            Lista Fumetti
+
+            {{-- <a href="{{ route('products.create') }}" class="btn btn-primary">
+                <i class="fas fa-plus"></i>
+                Aggiungi
+            </a> --}}
+        </h1>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Titolo</th>
+                    <th>Descrizione</th>
+                    <th>Immagine</th>
+                    <th>Prezzo</th>
+                    <th>Serie</th>
+                    <th>Data Uscita</th>
+                    <th>Tipo</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($comics as $comicEl)
+                    <tr>
+                        {{--
+                        $comics è un istanza del model comics.
+                        pertanto possiamo usare la freccia -> per leggere i
+                        valori delle varie colonne.
+                        Nulla vieta di usare le quadre [] come se fosse un
+                        array associativo
+                        --}}
+                        <td>{{ $comicEl['title'] }}</td>
+                        {{-- Taglio il testo in modo che abbia massimo 50 caratteri.
+                        Se ne ha di più vengono mostrati i ... --}}
+                        <td>{{ Str::limit($comicEl->description, 50) }}</td>
+                        <td>€ {{ $comicEl->price }}</td>
+                        <td>{{ $comicEl->series }}</td>
+                        <td>{{ $comicEl->sale_date }}</td>
+                        <td>{{ $comicEl->type }}</td>
+                        <td>
+                            {{-- La funzione route() crea l'url completo per arrivare su una pagina.
+                            Occore quindi passare il name delle rotta che vogliano.
+                            Siccome la rotta "show" si aspetta un parametro dinamico dell'uri, obbligatorio,
+                            questo lo passiamo come secondo argomento della funzione route(nome_rotta, valore_parametro_dinamico) --}}
+                            {{-- <a href="{{ route('products.show', $product->id) }}" class="btn btn-link">
+                                <i class="fas fa-eye"></i> --}}
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endsection
