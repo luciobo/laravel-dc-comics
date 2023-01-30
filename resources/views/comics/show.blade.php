3322921fb7199012b5c07cbd5bd2cf9b0c9819d0
luciobo <luciobo@icloud.com>
2023-01-30 16:53:24 +0100

    aggiunti butto elimina  e modifica su show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -12,7 +12,7 @@
             </div>
             <h1>{{ $comics->title }} (#{{ $comics->id }})</h1>
             <p class="l">{{ $comics->description }}</p>
-            <ul>
+            <ul class="m-0">
                 <li class="d-flex justify-content-center"><img class="w-25 py-2" src="{{ $comics->thumb }}"</li>
                 <li><strong>Prezzo:</strong> {{ $comics->price }}</li>
                 <li><strong>serie:</strong> {{ $comics->series }}</li>
@@ -21,6 +21,24 @@
                 <li><strong>Data creazione:</strong> {{ $comics->created_at }}</li>
                 <li><strong>Data ultima modifica:</strong> {{ $comics->updated_at }}</li>
             </ul>
+            <div class="d-flex justify-content-around p-3">
+                <div>
+                    <a class="" href="{{ route('comics.index') }}">
+                        <button type="button" class="btn btn-outline-light">modifica
+                            <i class="bi bi-pen-fill ps-2"></i></i>
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <form action="{{ route('comics.destroy', $comics->id) }}" method="POST">
+                        @csrf()
+                        @method('delete')
+                        <button class="btn btn-danger">Cancella
+                            <i class="bi bi-trash3 ps-3"></i></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
